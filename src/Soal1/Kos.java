0ecb91469b7fe29512f1cd57e09a3515b42ff74c
windarinjani53@gmail.com <windarinjani53@gmail.com>
2024-05-15 22:23:48 +0700

    Commit Kedua

--- src/Soal1/Kos.java
+++ src/Soal1/Kos.java
@@ -1,4 +1,29 @@
 package Soal1;
 
-public class Kos {
+
+class Kos {
+    private String namaKos;
+    private int hargaKos;
+
+    public Kos(String nama, int harga) {
+        namaKos = nama;
+        hargaKos = harga;
+    }
+
+    public void setNamaKos(String nama) {
+        namaKos = nama;
+    }
+
+    public String getNamaKos() {
+        return namaKos;
+    }
+
+    public void setHargaKos(int harga) {
+        hargaKos = harga;
+    }
+
+    public int getHargaKos() {
+        return hargaKos;
+    }
+
 }
--- src/Soal1/Main.java
+++ src/Soal1/Main.java
@@ -1,4 +1,20 @@
 package Soal1;
 
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama kos: ");
+        String nama = scanner.nextLine();
+
+        System.out.print("Masukkan harga kos per bulan: ");
+        int harga = scanner.nextInt();
+
+        Kos kos1 = new Kos(nama, harga);
+
+        kos1.setNamaKos("Kos Indah");
+        kos1.setHargaKos(500000);
+    }
 }
+
