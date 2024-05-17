bdd84dd9d6f617566a097593dcb6fc809e0714e4
windarinjani53@gmail.com <windarinjani53@gmail.com>
2024-05-17 21:57:00 +0700

    Commit keempat

---
+++ src/Soal4/ComplexFormula.java
@@ -0,0 +1,9 @@
+package Soal4;
+
+// Class untuk menghitung rumus kompleks
+class ComplexFormula {
+    // Constructor tanpa parameter
+    public ComplexFormula() {
+        System.out.println("Program Rumus Kompleks");
+    }
+}
